318cb5095ee2af33ec29ae598465b2460726558e
guimileib <guilhermemileib@gmail.com>
2026-05-24 23:36:15 -0300

    feat: Adiciona endpoints e modelo de mensagem ao aplicativo FastAPI

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,25 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fastapi_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá mundo!'}
+    return {"message": "Olá mundo!"}
+
+
+@app.get(
+    "/exercicio00", status_code=HTTPStatus.OK, response_class=HTMLResponse
+)
+def read_root2():
+    return """
+    <html>
+        <body>
+            <h1>Hello, World!</h1>
+        </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,26 @@
-from fastapi.testclient import TestClient
 from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
 from fastapi_zero.app import app
 
 
 def test_root_dev_retornar_hello_world():
-    '''
+    """
     Esse teste tem 3 etapas (AAA)
-    - A: Arrange - Arranjo - Carregar dados 
+    - A: Arrange - Arranjo - Carregar dados
     - A: Act - Chama o bloco de código executa (SUT)
     - A: Assert - Garanta que A é A
-    '''
+    """
 
     # Arrange
     client = TestClient(app)  # importou o client do fastapi
 
-    response = client.get('/')
+    response = client.get("/")
 
-    assert response.json() == {'message': 'Olá mundo!'}
+    assert response.json() == {"message": "Olá mundo!"}
     assert response.status_code == HTTPStatus.OK
 
+    response = client.get("/exercicio00")
+    assert response.status_code == HTTPStatus.OK
+    assert "<h1>Hello World!</h1>" in response.text
