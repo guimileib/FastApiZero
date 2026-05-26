e3439a9d40b394bb9fa085ce1551029729df98c8
guimileib <guilhermemileib@gmail.com>
2026-05-26 19:49:38 -0300

    feat: adiciona título à aplicação FastAPI e corrige a definição do modelo de usuário

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -2,19 +2,23 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-
+from fastapi_zero.schemas import UserSchema
 from fastapi_zero.schemas import Message
 
-app = FastAPI()
+app = FastAPI(title='FastAPI Succeed')
 
 
-@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
+@app.get("/", 
+        status_code=HTTPStatus.OK, 
+        response_model=Message)
 def read_root():
     return {"message": "Olá mundo!"}
 
 
 @app.get(
-    "/exercicio00", status_code=HTTPStatus.OK, response_class=HTMLResponse
+    "/exercicio00", 
+    status_code=HTTPStatus.OK,
+    response_class=HTMLResponse
 )
 def read_root2():
     return """
@@ -23,3 +27,11 @@ def read_root2():
             <h1>Hello, World!</h1>
         </body>
     </html>"""
+
+@app.post(
+    "/user", 
+    status_code=HTTPStatus.CREATED, 
+    response_model=UserSchema
+)
+def creat_user(user: UserSchema):
+    return user
--- fastapi_zero/schemas.py
+++ fastapi_zero/schemas.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 
 class Message(BaseModel):
     message: str
+
+class User(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
